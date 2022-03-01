c933ee3731b054040d25ff1de839e35d1eaab242
Zé Luís <jose.rodrigues@peakit.pt>
2022-03-01 23:55:31 +0000

    Add categories module

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const ContactController = require('./app/controllers/ContactController');
+const CategoryController = require('./app/controllers/CategoryController');
 
 const routes = Router();
 
@@ -8,5 +9,8 @@ routes.get('/contacts/:id', ContactController.show);
 routes.post('/contacts', ContactController.store);
 routes.delete('/contacts/:id', ContactController.delete);
 routes.put('/contacts/:id', ContactController.update);
+
+routes.get('/categories', CategoryController.index);
+routes.post('/categories', CategoryController.store);
 
 module.exports = routes;
