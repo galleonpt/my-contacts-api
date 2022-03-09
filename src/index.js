ecf67ebb5d9af7ac6a6bfb9a3399d2b72180419b
Zé Luís <jose.rodrigues@peakit.pt>
2022-03-09 22:57:34 +0000

    Add joins and error handler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+require('express-async-errors');
+
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use((error, request, response, next) => {
+  console.log(error);
+  console.log(error);
+  response.sendStatus(500);
+});
+
 app.listen(9999, () => console.log('Server running at http://localhost:9999'));
