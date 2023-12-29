0fd0ef90a2e18150c0d0c26ad4ec5d3bc6aa4640
galleonpt <jose15.luis00@gmail.com>
2023-12-29 14:10:31 +0000

    feat: allow multiple origins

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,7 +1,16 @@
 module.exports = (request, response, next) => {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  response.setHeader('Access-Control-Allow-Methods', '*');
-  response.setHeader('Access-Control-Allow-Headers', '*');
-  response.setHeader('Access-Control-Max-Age', '7200');
+  const originsWhiteList = [
+    'http://localhost:3000',
+  ];
+
+  const origin = request.header('Origin');
+  const isAllowed = originsWhiteList.includes(origin);
+
+  if (isAllowed) {
+    response.setHeader('Access-Control-Allow-Origin', origin);
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    response.setHeader('Access-Control-Max-Age', '7200');
+  }
   next();
 };
